45e03168630bab933880f9a3e0e871d5ee229e7e
cspark <git@cspark.dev>
2024-11-04 16:28:55 +0000

    Implement logic up to part 3

--- src/app.js
+++ src/app.js
@@ -3,9 +3,61 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async (request, response) => {
+	response.json(await Restaurant.findAll());
+});
 
+app.get("/restaurants/:id", async (request, response) => {
+	response.json(await Restaurant.findByPk(request.params.id));
+});
 
+app.post("/restaurants/:id", async (request, response) => {
+	const targetRestaurant = await Restaurant.findByPk(request.params.id);
+	if (targetRestaurant) {
+		response.status(400).json("Resource already exists!");
+		return;
+	};
+
+	const jsonRestaurant = Restaurant.toJSON();
+	const hasAllKeys = Object.keys(request.body).every((item) => { 
+		jsonRestaurant.hasOwnProperty(item);
+	})
+	if (hasAllKeys === false) {
+		response.status(400).json("Incorrect arguments provided!");	
+		return;
+	}
+
+	await Restaurant.create(request.body)
+	response.json("Resource successfulley created.");
+});
+
+app.update("/restaurants/:id", async (request, response) => {
+	const targetRestaurant = await Restaurant.findByPk(request.params.id);
+	const jsonRestaurant = Restaurant.toJSON();
+
+	for (const [key, value] of Object.entries(request.body)) {
+		if (key in jsonRestaurant) {
+			let updateTarget = {};
+			updateTarget[key] = value;
+			await targetRestaurant.update(updateTarget);
+		} else {
+			response.status(400).json(`Argument "${key}: ${value}" provided do not exist!`);	
+		}
+	}
+
+	response.json("Resource successfully updated.");
+});
+
+app.delete("/restaurants/:id", async (request, response) => {
+	const targetRestaurant = await Restaurant.findByPk(request.params.id);
+	await targetRestaurant.destroy();
+
+	response.json(`Resource successfully deleted.`);
+});
 
 
 module.exports = app;
