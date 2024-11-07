f53fc352710300502bde907861c1d0d3c4fc4656
cspark <git@cspark.dev>
2024-11-07 14:54:38 +0000

    Implement validation

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const db = require("../db/connection");
 
 app.use(express.json());
 app.use(express.urlencoded());
+const {check, validationResult} = require("express-validator");
 
 //TODO: Create your GET Request Route Below: 
 app.get("/restaurants", async (request, response) => {
@@ -15,25 +16,38 @@ app.get("/restaurants/:id", async (request, response) => {
 	response.json(await Restaurant.findByPk(request.params.id));
 });
 
-app.post("/restaurants/:id", async (request, response) => {
-	const targetRestaurant = await Restaurant.findByPk(request.params.id);
-	if (targetRestaurant) {
-		response.status(400).json("Resource already exists!");
-		return;
-	};
-
-	const jsonRestaurant = Restaurant.toJSON();
-	const hasAllKeys = Object.keys(request.body).every((item) => { 
-		jsonRestaurant.hasOwnProperty(item);
-	})
-	if (hasAllKeys === false) {
-		response.status(400).json("Incorrect arguments provided!");	
-		return;
+app.post("/restaurants/:id",	
+	[
+		check("name").notEmpty().trim(), 
+		check("location").notEmpty().trim(),
+		check("cuisine").notEmpty().trim()
+	], 
+	async (request, response) => {
+		const errors = validationResult(request);
+		if (errors.isEmpty() === false) {
+			response.json({ error: errors.array });	
+			return;
+		}
+	
+		const targetRestaurant = await Restaurant.findByPk(request.params.id);
+		if (targetRestaurant) {
+			response.status(400).json("Resource already exists!");
+			return;
+		};
+	
+		const jsonRestaurant = Restaurant.toJSON();
+		const hasAllKeys = Object.keys(request.body).every((item) => { 
+			jsonRestaurant.hasOwnProperty(item);
+		})
+		if (hasAllKeys === false) {
+			response.status(400).json("Incorrect arguments provided!");	
+			return;
+		}
+	
+		await Restaurant.create(request.body)
+		response.json("Resource successfulley created.");
 	}
-
-	await Restaurant.create(request.body)
-	response.json("Resource successfulley created.");
-});
+);
 
 app.put("/restaurants/:id", async (request, response) => {
 	const targetRestaurant = await Restaurant.findByPk(request.params.id);
